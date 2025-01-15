eb3e203ceb5c72ce327b3927664a6bf3fd6634c1
seongjju <jeongsj1226@naver.com>
2025-01-15 15:19:45 +0900

    app.py 수정r

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-open_csv = st.Page("open_csv.py", title="Open Your csv file")
+# open_csv = st.Page("open_csv.py", title="Open Your csv file")
 add_menu = st.Page("add_menu.py", title="Query Your Data")
 create_table_app = st.Page("create_table_app.py", title="Create Table with CSV file")
-pg = st.navigation([open_csv, add_menu, create_table_app])
+pg = st.navigation([add_menu, create_table_app])
 pg.run()
 
